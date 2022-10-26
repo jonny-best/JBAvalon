a48105dcdec788d091dd3bedf8a4e58c96494dcd
Evgeny <112688864+jonny-best@users.noreply.github.com>
2022-10-26 16:52:06 +0300

    Лабораторная 4 _ черновик
    Первую задачу доделал. Вторую только частично. Последний print не получается. Помню, что там какие-то ошибки были. Но не могу в записи практики найти где про ошибки говорили.

---
+++ Лабораторная работа 4/main_task2.py
@@ -0,0 +1,7 @@
+def delete(list_, index=None):
+    list_.remove(index)  # TODO реализовать функцию удаления элемента из списка по индексу
+    return list_
+
+print(delete([0, 0, 1, 2], index=0))  # [0, 1]
+print(delete([0, 1, 1, 2, 3], index=1))  # [0, 1, 2]
+print(delete([0, 1, 2, 3, 4, 4]))  # [0, 1, 2, 3]
